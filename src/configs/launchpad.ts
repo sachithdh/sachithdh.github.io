ec7909416fa7bfefed018ae80e06eaa585515591
sacheex <sachithdhanushka00@gmail.com>
2024-07-26 10:10:49 +0530

    Add Projects

--- src/configs/launchpad.ts
+++ src/configs/launchpad.ts
@@ -3,64 +3,28 @@ import type { LaunchpadData } from "~/types";
 const launchpadApps: LaunchpadData[] = [
   {
     id: "wpa2hunter",
-    title: "Wpa2hunter",
-    img: "img/icons/launchpad/flint.png",
-    link: "https://github.com/sacheex/WPA2-WIFI-Password-Cracker"
+    title: "Wpa2Hunter",
+    img: "img/icons/launchpad/wpa2hunter.png",
+    link: "https://github.com/sachithdh/WPA2Hunter"
+  },
+  {
+    id: "geminicli",
+    title: "GeminiCLI",
+    img: "img/icons/launchpad/geminicli.png",
+    link: "https://github.com/sachithdh/GeminiCLI"
+  },
+  {
+    id: "tranxcribe",
+    title: "tranXcribe",
+    img: "img/icons/launchpad/tranxcribe.png",
+    link: "https://github.com/sachithdh/tranXcribe"
+  },
+  {
+    id: "tranxcribecli",
+    title: "tranxcribe-CLI",
+    img: "img/icons/launchpad/tranxcribecli.png",
+    link: "https://github.com/sachithdh/tranXcribe-CLI"
   }
-  // {
-  //   id: "oh-my-cv",
-  //   title: "Oh, My CV!",
-  //   img: "https://ohmycv.app/pwa-512x512.png",
-  //   link: "https://ohmycv.app"
-  // },
-  // {
-  //   id: "oh-vue-icons",
-  //   title: "Oh, Vue Icons!",
-  //   img: "img/icons/launchpad/oh-vue-icons.png",
-  //   link: "https://oh-vue-icons.js.org"
-  // },
-  // {
-  //   id: "gungnir",
-  //   title: "Gungnir",
-  //   img: "img/icons/launchpad/gungnir.png",
-  //   link: "https://vuepress-theme-gungnir.vercel.app/"
-  // },
-  // // {
-  // //   id: "metallic",
-  // //   title: "Metallic",
-  // //   img: "img/icons/launchpad/meta.png",
-  // //   link: "https://github.com/Renovamen/metallic"
-  // // },
-  // {
-  //   id: "what-if",
-  //   title: "My Notebook",
-  //   img: "img/icons/launchpad/notebook.png",
-  //   link: "https://note.zxh.io"
-  // },
-  // {
-  //   id: "cube",
-  //   title: "Cube Solver",
-  //   img: "img/icons/launchpad/cube.png",
-  //   link: "https://cube.zxh.io/"
-  // },
-  // {
-  //   id: "resume",
-  //   title: "Résumé",
-  //   img: "img/icons/launchpad/resume.png",
-  //   link: "https://resume.zxh.io/"
-  // },
-  // {
-  //   id: "zelda",
-  //   title: "Webpage for Zelda",
-  //   img: "img/icons/launchpad/zelda.png",
-  //   link: "https://zelda.zxh.io/"
-  // },
-  // {
-  //   id: "fishmail",
-  //   title: "Fishmail",
-  //   img: "img/icons/launchpad/fishmail.png",
-  //   link: "https://fishmail.vercel.app/"
-  // }
 ];
 
 export default launchpadApps;
